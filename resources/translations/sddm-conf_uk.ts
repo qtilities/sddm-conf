563bd8d86f4562ba00d740870f383ef517755481
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2022-04-19 20:39:56 +0200

    Weblate commits (#11)
    Co-authored-by: Baptiste H <baptiste.huchon@bechamail.fr>
    Co-authored-by: Ihor Hordiichuk <igor_ck@outlook.com>
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    
    Translate-URL: https://translate.lxqt-project.org/projects/redtide/sddm-conf-menu-entry/et/
    Translate-URL: https://translate.lxqt-project.org/projects/redtide/sddm-conf-menu-entry/fr/
    Translate-URL: https://translate.lxqt-project.org/projects/redtide/sddm-conf/et/
    Translate-URL: https://translate.lxqt-project.org/projects/redtide/sddm-conf/fr/
    Translate-URL: https://translate.lxqt-project.org/projects/redtide/sddm-conf/uk/
    Translation: redtide/SDDM-conf
    Translation: redtide/SDDM-conf Menu Entry

--- resources/translations/sddm-conf_uk.ts
+++ resources/translations/sddm-conf_uk.ts
@@ -183,7 +183,7 @@ unless explicitly enabled with EnableAvatars</source>
     <message>
         <location filename="../../src/maindialog.ui" line="572"/>
         <source>The lowest virtual terminal number that will be used</source>
-        <translation type="unfinished"></translation>
+        <translation>Найнижчий номер віртуального термінала, який буде використаний</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="669"/>
@@ -233,7 +233,7 @@ unless explicitly enabled with EnableAvatars</source>
     <message>
         <location filename="../../src/maindialog.ui" line="778"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Про застосунок</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.cpp" line="23"/>
@@ -243,28 +243,29 @@ unless explicitly enabled with EnableAvatars</source>
     <message>
         <location filename="../../src/maindialog.cpp" line="61"/>
         <source>SDDM Conf v</source>
-        <translation type="unfinished"></translation>
+        <translation>SDDM Conf v</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.cpp" line="62"/>
         <source>Copyright (C) 2021
 Andrea Zanellato &lt;redtid3@gmail.com&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Авторське право (C) 2021
+Andrea Zanellato &lt;redtid3@gmail.com&gt;</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.cpp" line="72"/>
         <source>Close %1 preview</source>
-        <translation type="unfinished"></translation>
+        <translation>Закрити %1 попередній перегляд</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.cpp" line="87"/>
         <source>Choose a file</source>
-        <translation type="unfinished"></translation>
+        <translation>Вибрати файл</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.cpp" line="94"/>
         <source>Choose a directory</source>
-        <translation type="unfinished"></translation>
+        <translation>Вибрати каталог</translation>
     </message>
 </context>
 </TS>
