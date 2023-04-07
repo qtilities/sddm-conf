6f487d271afa350594917e75f1b047498f3f2065
Weblate <noreply@weblate.org>
2023-04-07 17:11:29 +0200

    Weblate commits
    Co-authored-by: Jouni Järvinen <jounijarvis@gmail.com>
    Co-authored-by: Moo <hazap@hotmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/redtide/sddm-conf-menu-entry/fi/
    Translate-URL: https://translate.lxqt-project.org/projects/redtide/sddm-conf-menu-entry/lt/
    Translate-URL: https://translate.lxqt-project.org/projects/redtide/sddm-conf/fi/
    Translate-URL: https://translate.lxqt-project.org/projects/redtide/sddm-conf/lt/
    Translation: redtide/SDDM-conf
    Translation: redtide/SDDM-conf Menu Entry

---
+++ resources/translations/sddm-conf_fi.ts
@@ -0,0 +1,216 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fi">
+<context>
+    <name>MainDialog</name>
+    <message>
+        <source>Autologin</source>
+        <translation>Automaattikirjautuminen</translation>
+    </message>
+    <message>
+        <source>Username for autologin session</source>
+        <translation>Käyttäjätunnus automaattikirjautumiseen</translation>
+    </message>
+    <message>
+        <source>Whether sddm should automatically log back into sessions when they exit</source>
+        <translation>Pitäisikö sddm kirjautua automaattisesti takaisin sessioihin, kun ne päätetään</translation>
+    </message>
+    <message>
+        <source>Name of session file for autologin session (if empty try last logged in)</source>
+        <translation>Sessiotiedosto automaattikirjautumista varten (tyhjä yrittää viimeiseksi kirjautunutta)</translation>
+    </message>
+    <message>
+        <source>General</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Reboot command</source>
+        <translation>Uudelleenkäynnistyskomento</translation>
+    </message>
+    <message>
+        <source>If property is set to none, numlock won&apos;t be changed
+NOTE: Currently ignored if autologin is enabled.</source>
+        <translation>Jos ei valittu, numlockkia ei muuteta
+HUOM: Ei vaikutusta jos automaattikirjautuminen on käytössä</translation>
+    </message>
+    <message>
+        <source>Initial NumLock state. Can be on, off or none.</source>
+        <translation>NumLockin alkutila. Voi olla päällä, pois tai määrittelemätön.</translation>
+    </message>
+    <message>
+        <source>Input method module</source>
+        <translation>Syötemenetelmämoduuli</translation>
+    </message>
+    <message>
+        <source>Halt command</source>
+        <translation>Pysäytyskomento</translation>
+    </message>
+    <message>
+        <source>Theme</source>
+        <translation>Teema</translation>
+    </message>
+    <message>
+        <source>Theme directory path</source>
+        <translation>Teemahakemistopolku</translation>
+    </message>
+    <message>
+        <source>above which avatars are disabled
+unless explicitly enabled with EnableAvatars</source>
+        <translation>yllä olevaa ylittävät avatarit eivät ole käytössä
+jos ei erikseen määritetty EnableAvatars-asetuksella</translation>
+    </message>
+    <message>
+        <source>Current theme name</source>
+        <translation>Nykyisen teeman nimi</translation>
+    </message>
+    <message>
+        <source>Cursor theme used in the greeter</source>
+        <translation>Tervehdyksessä käytetty osoitinteema</translation>
+    </message>
+    <message>
+        <source>Global directory for user avatars</source>
+        <translation>Yleinen hakemisto avatareille</translation>
+    </message>
+    <message>
+        <source>Number of users to use as threshold</source>
+        <translation>Raja-arvona käytettävä käyttäjälukumäärä</translation>
+    </message>
+    <message>
+        <source>Preview</source>
+        <translation>Esikatselu</translation>
+    </message>
+    <message>
+        <source>Enable display of custom user avatars</source>
+        <translation>Näytä mukautetut avatarit</translation>
+    </message>
+    <message>
+        <source>The files should be named &lt;username&gt;.face.icon</source>
+        <translation>Tiedostot täytyy nimetä &lt;käyttäjänimi&gt;.face.icon</translation>
+    </message>
+    <message>
+        <source>Users</source>
+        <translation>Käyttäjät</translation>
+    </message>
+    <message>
+        <source>Default $PATH for logged in users</source>
+        <translation>$PATH-oletusmuuttuja kirjautuneille käyttäjille</translation>
+    </message>
+    <message>
+        <source>Comma-separated list of shells</source>
+        <translation>Pilkkuerotettu luettelo komentotulkeista</translation>
+    </message>
+    <message>
+        <source>Comma-separated list of users that should not be listed</source>
+        <translation>Pilkkuerotettu luettelo käyttäjistä, joita ei listata</translation>
+    </message>
+    <message>
+        <source>Minimum user id for displayed users</source>
+        <translation>Alin näytettävä käyttäjänumero</translation>
+    </message>
+    <message>
+        <source>Maximum user id for displayed users</source>
+        <translation>Korkein näytettävä käyttäjänumero</translation>
+    </message>
+    <message>
+        <source>Remember the session of the last successfully logged in user</source>
+        <translation>Muista viimeisen käyttäjän toimiva sessio</translation>
+    </message>
+    <message>
+        <source>Remember the last successfully logged in user</source>
+        <translation>Muistava viimeinen toimiva käyttäjä</translation>
+    </message>
+    <message>
+        <source>Wayland</source>
+        <translation>Wayland</translation>
+    </message>
+    <message>
+        <source>Enable Qt&apos;s automatic high-DPI scaling</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Path to a script to execute when starting the desktop session</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Path to the user session log file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Directory containing available Wayland sessions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>X11</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The lowest virtual terminal number that will be used</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Path to X server binary</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Arguments passed to the X server invocation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Directory containing available X sessions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Path to a script to execute when starting the display server</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Path to a script to execute when stopping the display server</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Path to xauth binary</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Path to Xephyr binary</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Path to the Xauthority file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>SDDM Configuration Editor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>SDDM Conf v</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Copyright (C) 2021
+Andrea Zanellato &lt;redtid3@gmail.com&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Choose a file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Choose a directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Close %1 preview</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
