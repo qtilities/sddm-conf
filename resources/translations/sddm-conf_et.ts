3fc97406be2c7e5b73515f4904ccd4237bd65553
Weblate <noreply@weblate.org>
2023-10-01 18:22:32 +0200

    Weblate commits
    Co-authored-by: Baptiste Huchon <baptiste.huchon@ikmail.com>
    Co-authored-by: Heimen Stoffels <vistausss@fastmail.com>
    Co-authored-by: Ihor Hordiichuk <igor_ck@outlook.com>
    Co-authored-by: Jan Rolski <wbcwknvstb@proton.me>
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/sddm-conf-menu-entry/et/
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/sddm-conf-menu-entry/fr/
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/sddm-conf-menu-entry/pl/
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/sddm-conf/et/
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/sddm-conf/nl/
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/sddm-conf/pl/
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/sddm-conf/uk/
    Translation: Qtilities/SDDM-conf
    Translation: Qtilities/SDDM-conf Menu Entry

--- resources/translations/sddm-conf_et.ts
+++ resources/translations/sddm-conf_et.ts
@@ -261,42 +261,42 @@ välja arvatud olukord, kui EnableAvatars nad üheselt kasutusele võtab</transl
     <message>
         <location filename="../../src/dialogabout.ui" line="27"/>
         <source>Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Teave</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="43"/>
         <source>qrc:/about.html</source>
-        <translation type="unfinished"></translation>
+        <translation>qrc:/about.html</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="62"/>
         <source>Thanks</source>
-        <translation type="unfinished"></translation>
+        <translation>Suur tänu</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="81"/>
         <source>qrc:/thanks.html</source>
-        <translation type="unfinished"></translation>
+        <translation>qrc:/thanks.html</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="100"/>
         <source>License</source>
-        <translation type="unfinished"></translation>
+        <translation>Litsents</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="116"/>
         <source>qrc:/license.html</source>
-        <translation type="unfinished"></translation>
+        <translation>qrc:/license.html</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.cpp" line="55"/>
         <source>Author</source>
-        <translation type="unfinished"></translation>
+        <translation>Autor</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.cpp" line="60"/>
         <source>About</source>
-        <translation type="unfinished">Rakenduse teave</translation>
+        <translation>Rakenduse teave</translation>
     </message>
 </context>
 </TS>
