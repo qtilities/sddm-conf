15a8070a7f37bbcb76714cf53e2f4a5d0bce87de
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2024-11-08 07:33:00 +0100

    Weblate commits (#52)
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/sddm-conf-menu-entry/ca/
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/sddm-conf-menu-entry/nb_NO/
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/sddm-conf/ca/
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/sddm-conf/fi/
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/sddm-conf/nb_NO/
    Translation: Qtilities/SDDM-conf
    Translation: Qtilities/SDDM-conf Menu Entry
    
    Co-authored-by: Davidmp <opensusecatala@gmail.com>
    Co-authored-by: Jouni Järvinen <jounijarvis@gmail.com>
    Co-authored-by: ovl-1 <ovl-002@mailinator.com>

--- resources/translations/sddm-conf_ca.ts
+++ resources/translations/sddm-conf_ca.ts
@@ -261,42 +261,42 @@ tret que estigui activat explícitament amb EnableAvatars</translation>
     <message>
         <location filename="../../src/dialogabout.ui" line="27"/>
         <source>Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Informació</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="43"/>
         <source>qrc:/about.html</source>
-        <translation type="unfinished"></translation>
+        <translation>qrc:/about.html</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="62"/>
         <source>Thanks</source>
-        <translation type="unfinished"></translation>
+        <translation>Mercès</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="81"/>
         <source>qrc:/thanks.html</source>
-        <translation type="unfinished"></translation>
+        <translation>qrc:/thanks.html</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="100"/>
         <source>License</source>
-        <translation type="unfinished"></translation>
+        <translation>Llicència</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="116"/>
         <source>qrc:/license.html</source>
-        <translation type="unfinished"></translation>
+        <translation>qrc:/license.html</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.cpp" line="55"/>
         <source>Author</source>
-        <translation type="unfinished"></translation>
+        <translation>Autor</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.cpp" line="60"/>
         <source>About</source>
-        <translation type="unfinished">Quant a</translation>
+        <translation>Quant a</translation>
     </message>
 </context>
 </TS>
--- resources/translations/sddm-conf_fi.ts
+++ resources/translations/sddm-conf_fi.ts
@@ -261,7 +261,7 @@ jos ei erikseen määritetty EnableAvatars-asetuksella</translation>
     <message>
         <location filename="../../src/dialogabout.ui" line="27"/>
         <source>Information</source>
-        <translation>Tiedot</translation>
+        <translation type="unfinished">Tiedot</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="43"/>
