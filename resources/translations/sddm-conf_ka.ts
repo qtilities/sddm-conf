f21fb524d597f0d7ea44afb82f566b1400a3a4bb
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2025-01-03 08:34:31 +0100

    Weblate commits (#54)
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/sddm-conf-menu-entry/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/sddm-conf/ka/
    Translation: Qtilities/SDDM-conf
    Translation: Qtilities/SDDM-conf Menu Entry
    
    Co-authored-by: NorwayFun <temuri.doghonadze@gmail.com>

---
+++ resources/translations/sddm-conf_ka.ts
@@ -0,0 +1,302 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ka">
+<context>
+    <name>MainDialog</name>
+    <message>
+        <location filename="../../src/maindialog.ui" line="40"/>
+        <source>Autologin</source>
+        <translation>ავტომატური შესვლა</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="61"/>
+        <source>Username for autologin session</source>
+        <translation>მომხმარებლის სახელი სესიაში ავტოშესვლისთვის</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="71"/>
+        <source>Whether sddm should automatically log back into sessions when they exit</source>
+        <translation>უნდა შევიდეს თუ არა sddm ისევ სესიაში, როცა ის დასრულდება</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="84"/>
+        <source>Name of session file for autologin session (if empty try last logged in)</source>
+        <translation>სესიის ფაილის სახელი ავტოშესვლის სესიისთვის (თუ ცარიელია, გამოიყენება ბოლოს შესული მომხმარებელი)</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="108"/>
+        <source>General</source>
+        <translation>ზოგადი</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="135"/>
+        <source>Reboot command</source>
+        <translation>გადატვრთვის ბრძანება</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="142"/>
+        <source>If property is set to none, numlock won&apos;t be changed
+NOTE: Currently ignored if autologin is enabled.</source>
+        <translation>თუ თვისება დაყენებული არაა, NumLock არ შეიცვლება.
+შენიშვნა: ამჟამად გამოტოვებულია, თუ ავტოშესვლა ჩართულია.</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="172"/>
+        <source>Initial NumLock state. Can be on, off or none.</source>
+        <translation>საწყისი NumLock-ის მდგომარეობა. შეიძლება იყოს ჩართული, გამორთული ან არცერთი.</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="179"/>
+        <source>Input method module</source>
+        <translation>შეყვანის მეთოდის მოდული</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="186"/>
+        <source>Halt command</source>
+        <translation>გამორთვის ბრძანება</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="207"/>
+        <source>Theme</source>
+        <translation>თემა</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="250"/>
+        <source>Theme directory path</source>
+        <translation>თემის საქაღალდის ბილიკი</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="257"/>
+        <source>above which avatars are disabled
+unless explicitly enabled with EnableAvatars</source>
+        <translation>რომლის ზემოთაც ავატარები გამორთულია,
+თუ აშკარად არაა ჩართული ბრძანებით EnableAvatars</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="265"/>
+        <source>Current theme name</source>
+        <translation>მიმდინარე თემის სახელი</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="272"/>
+        <source>Cursor theme used in the greeter</source>
+        <translation>კურსორის თემა, რომელსაც მისალმების პროგრამა იყენებს</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="279"/>
+        <source>Global directory for user avatars</source>
+        <translation>გლობალური საქაღალდე მომხმარებლის ავატარებისთვის</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="289"/>
+        <source>Number of users to use as threshold</source>
+        <translation>მომხმარებლების რაოდენობის ზღვარი</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="302"/>
+        <source>Preview</source>
+        <translation>მინიატურა</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="309"/>
+        <source>Enable display of custom user avatars</source>
+        <translation>მორგებული მომხმარებლის ავატარების ჩვენების ჩართვა</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="316"/>
+        <source>The files should be named &lt;username&gt;.face.icon</source>
+        <translation>ფაილს უნდა ერქვას &lt;მომხმარებლისსახელი&gt;.face.icon</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="324"/>
+        <source>Users</source>
+        <translation>მომხმარებლები</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="345"/>
+        <source>Default $PATH for logged in users</source>
+        <translation>ნაგულისხმევი $PATH შესული მომხმარებლებისთვის</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="355"/>
+        <source>Comma-separated list of shells</source>
+        <translation>მძიმით გამოტოვებული გარსების სია</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="365"/>
+        <source>Comma-separated list of users that should not be listed</source>
+        <translation>მძიმით გამოტოვებული სია მომხმარებლებისა, რომელიც სიაში არ უნდა გამოჩნდეს</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="375"/>
+        <source>Minimum user id for displayed users</source>
+        <translation>ნაჩვენები მომხმარებლების მინიმალური UID</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="392"/>
+        <source>Maximum user id for displayed users</source>
+        <translation>მაქსიმალური მომხმარებლის ID ნაჩვენები მომხმარებლებისთვის</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="409"/>
+        <source>Remember the session of the last successfully logged in user</source>
+        <translation>ბოლოს წარმატებით შესული მომხმარებლის სესიის დამახსოვრება</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="416"/>
+        <source>Remember the last successfully logged in user</source>
+        <translation>ბოლოს წარმატებით შესული მომხმარებლის დამახსოვრება</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="424"/>
+        <source>Wayland</source>
+        <translation>Wayland</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="445"/>
+        <location filename="../../src/maindialog.ui" line="565"/>
+        <source>Enable Qt&apos;s automatic high-DPI scaling</source>
+        <translation>Qt-ის ავტომატური მაღალი-DPI-ის დამასშტაბების ჩართვა</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="452"/>
+        <location filename="../../src/maindialog.ui" line="676"/>
+        <source>Path to a script to execute when starting the desktop session</source>
+        <translation>ბილიკი სკრიპტამდე, რომელიც სამუშაო მაგიდის სესიის გაშვებისას შესრულდება</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="470"/>
+        <location filename="../../src/maindialog.ui" line="740"/>
+        <source>Path to the user session log file</source>
+        <translation>ბილიკი მომხმარებლის სესიის ჟურნალის ფაილამდე</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="488"/>
+        <source>Directory containing available Wayland sessions</source>
+        <translation>ხელმისაწვდომი Wayland-ის სესიების შემცველი საქაღალდე</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="520"/>
+        <source>X11</source>
+        <translation>X11</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="572"/>
+        <source>The lowest virtual terminal number that will be used</source>
+        <translation>უმცირესი ვირტუალური ტერმინალის ნომერი, რომელიც გამოყენებული იქნება</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="669"/>
+        <source>Path to X server binary</source>
+        <translation>ბილიკი X სერვერის გამშვებ ფაილამდე</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="683"/>
+        <source>Arguments passed to the X server invocation</source>
+        <translation>გადაცემული არგუმენტები X სერვერის გაშვებისას</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="690"/>
+        <source>Directory containing available X sessions</source>
+        <translation>ხელმისაწვდომი X სესიების შემცველი საქაღალდე</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="697"/>
+        <source>Path to a script to execute when starting the display server</source>
+        <translation>ბილიკი ეკრანის სერვერის გაშვებისას შესასრულებელ სკრიპტამდე</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="704"/>
+        <source>Path to a script to execute when stopping the display server</source>
+        <translation>ბილიკი ეკრანის სერვერის გაჩერებისას შესასრულებელ სკრიპტამდე</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="714"/>
+        <source>Path to xauth binary</source>
+        <translation>ბილიკი გამშვებ ფაილამდე xauth</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="721"/>
+        <source>Path to Xephyr binary</source>
+        <translation>ბილიკი გამშვებ ფაილამდე Xephyr</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="747"/>
+        <source>Path to the Xauthority file</source>
+        <translation>ბილიკი ფაილამდე Xauthority</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="759"/>
+        <source>File</source>
+        <translation>ფაილი</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.cpp" line="31"/>
+        <source>SDDM Configuration Editor</source>
+        <translation>SDDM-ის კონფიგურაციის რედაქტორი</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.ui" line="778"/>
+        <source>About</source>
+        <translation>შესახებ</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.cpp" line="89"/>
+        <source>Choose a file</source>
+        <translation>ფაილის არჩევა</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.cpp" line="96"/>
+        <source>Choose a directory</source>
+        <translation>აირჩიეთ საქაღალდე</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.cpp" line="74"/>
+        <source>Close %1 preview</source>
+        <translation>%1 მინიატურის დახურვა</translation>
+    </message>
+</context>
+<context>
+    <name>Qtilities::DialogAbout</name>
+    <message>
+        <location filename="../../src/dialogabout.ui" line="27"/>
+        <source>Information</source>
+        <translation>ინფორმაცია</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.ui" line="43"/>
+        <source>qrc:/about.html</source>
+        <translation>qrc:/about.html</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.ui" line="62"/>
+        <source>Thanks</source>
+        <translation>მადლობა</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.ui" line="81"/>
+        <source>qrc:/thanks.html</source>
+        <translation>qrc:/thanks.html</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.ui" line="100"/>
+        <source>License</source>
+        <translation>ლიცენზია</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.ui" line="116"/>
+        <source>qrc:/license.html</source>
+        <translation>qrc:/license.html</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.cpp" line="55"/>
+        <source>Author</source>
+        <translation>ავტორი</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.cpp" line="60"/>
+        <source>About</source>
+        <translation>შესახებ</translation>
+    </message>
+</context>
+</TS>
