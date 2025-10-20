435819f27802204f9e89cff4eb3a7983a7ac7b01
Weblate <noreply@weblate.org>
2025-10-20 06:27:41 +0200

    Weblate commits
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: p-bo <pavel.borecki@gmail.com>
    Co-authored-by: virtual-hand <handvirtual@tutamail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/sddm-conf-menu-entry/cs/
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/sddm-conf-menu-entry/pt_BR/
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/sddm-conf/cs/
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/sddm-conf/pt_BR/
    Translation: Qtilities/SDDM-conf
    Translation: Qtilities/SDDM-conf Menu Entry

--- resources/translations/sddm-conf_pt_BR.ts
+++ resources/translations/sddm-conf_pt_BR.ts
@@ -1,179 +1,178 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="cs">
+<TS version="2.1" language="pt_BR">
 <context>
     <name>MainDialog</name>
     <message>
         <location filename="../../src/maindialog.ui" line="40"/>
         <source>Autologin</source>
-        <translation>Automatické přihlášení</translation>
+        <translation>Login automático</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="61"/>
         <source>Username for autologin session</source>
-        <translation>Uživatelské jméno pro automaticky přihlášené sezení</translation>
+        <translation>Usuário para login automático</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="71"/>
         <source>Whether sddm should automatically log back into sessions when they exit</source>
-        <translation>Zda se má sddm automaticky přihlásit zpět k sezením po jejich ukončení</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="84"/>
         <source>Name of session file for autologin session (if empty try last logged in)</source>
-        <translation>Název souboru se sezením pro automaticky přihlášené sezení (pokud je prázdný, zkusit poslední přihlášení)</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="108"/>
         <source>General</source>
-        <translation>Obecné</translation>
+        <translation>Geral</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="135"/>
         <source>Reboot command</source>
-        <translation>Příkaz pro restart</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="142"/>
         <source>If property is set to none, numlock won&apos;t be changed
 NOTE: Currently ignored if autologin is enabled.</source>
-        <translation>Pokud je vlastnost nastavena na žádné, stav numerické klávesnice nebude změněn
-POZN.: V současnosti je ignorováno, pokud je zapnuté automatické přihlašování.</translation>
+        <translation>Se essa opção for definida para nenhum, numlock não será modificado 
+Nota: Ignorado se o login automático está habilitado.</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="172"/>
         <source>Initial NumLock state. Can be on, off or none.</source>
-        <translation>Počáteční stav zámku klávesnice (NumLock). Může být zapnuta, vypnuta nebo žádný.</translation>
+        <translation>Estado inicial de NumLock. Pode ser ligado, ativado ou nenhum.</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="179"/>
         <source>Input method module</source>
-        <translation>Modul metody zadávání</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="186"/>
         <source>Halt command</source>
-        <translation>Příkaz pro zastavení</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="207"/>
         <source>Theme</source>
-        <translation>Motiv vzhledu</translation>
+        <translation>Tema</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="250"/>
         <source>Theme directory path</source>
-        <translation>Popis umístění složky s motivy vzhledu</translation>
+        <translation>Diretório do tema</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="257"/>
         <source>above which avatars are disabled
 unless explicitly enabled with EnableAvatars</source>
-        <translation>nad který jsou avataři vypnutí
-pokud nejsou výslovně zapnutí pomocí EnableAvatars</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="265"/>
         <source>Current theme name</source>
-        <translation>Název stávajícího motivu vzhledu</translation>
+        <translation>Nome do tema atual</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="272"/>
         <source>Cursor theme used in the greeter</source>
-        <translation>Uživatelsky určený motiv vzhledu uvítací obrazovky</translation>
+        <translation type="unfinished">Tema do cursor na tela de boas-vindas</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="279"/>
         <source>Global directory for user avatars</source>
-        <translation>Globální složka pro avatary uživatelů</translation>
+        <translation>Diretório global para avatares de usuário</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="289"/>
         <source>Number of users to use as threshold</source>
-        <translation>Počet uživatelů, který použít jako práh</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="302"/>
         <source>Preview</source>
-        <translation>Náhled</translation>
+        <translation>Prever</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="309"/>
         <source>Enable display of custom user avatars</source>
-        <translation>Zapnout zobrazování uživatelsky určených avatarů uživatelů</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="316"/>
         <source>The files should be named &lt;username&gt;.face.icon</source>
-        <translation>Soubory by měly mít názvy &lt;username&gt;.face.icon</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="324"/>
         <source>Users</source>
-        <translation>Uživatelé</translation>
+        <translation>Usuários</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="345"/>
         <source>Default $PATH for logged in users</source>
-        <translation>Výchozí obsah proměnné $PATH pro přihlašující se uživatele</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="355"/>
         <source>Comma-separated list of shells</source>
-        <translation>Čárkami oddělovaný seznam shellů</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="365"/>
         <source>Comma-separated list of users that should not be listed</source>
-        <translation>Čárkou oddělovaný seznam uživatelů, kteří nebudou vypsáni</translation>
+        <translation>Lista de usuários separados por vírgula que não deveriam ser listados</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="375"/>
         <source>Minimum user id for displayed users</source>
-        <translation>Nejnižší identif. uživatele pro zobrazované uživatele</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="392"/>
         <source>Maximum user id for displayed users</source>
-        <translation>Nejvyšší identif. uživatele pro zobrazované uživatele</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="409"/>
         <source>Remember the session of the last successfully logged in user</source>
-        <translation>Pamatovat si relaci naposledy úspěšně přihlášeného uživatele</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="416"/>
         <source>Remember the last successfully logged in user</source>
-        <translation>Pamatovat si naposledy úspěšně přihlášeného uživatele</translation>
+        <translation>Lembrar o último usuário logado</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="424"/>
         <source>Wayland</source>
-        <translation>Wayland</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="445"/>
         <location filename="../../src/maindialog.ui" line="565"/>
         <source>Enable Qt&apos;s automatic high-DPI scaling</source>
-        <translation>Zapnout automatické škálování HDPI v rámci Qt</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="452"/>
         <location filename="../../src/maindialog.ui" line="676"/>
         <source>Path to a script to execute when starting the desktop session</source>
-        <translation>Popis umístění skriptu který vykonat při spouštění relace plochy</translation>
+        <translation>Caminho para um script a ser executado ao iniciar uma sessão</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="470"/>
         <location filename="../../src/maindialog.ui" line="740"/>
         <source>Path to the user session log file</source>
-        <translation>Popis umístění souboru se záznamem událostí relace uživatele</translation>
+        <translation>Caminho para o arquivo de log da sessão do usuário</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="488"/>
         <source>Directory containing available Wayland sessions</source>
-        <translation>Složka obsahující Wayland relace, které jsou k dispozici</translation>
+        <translation>Diretório contendo sessões de Wayland disponíveis</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="520"/>
@@ -183,77 +182,77 @@ pokud nejsou výslovně zapnutí pomocí EnableAvatars</translation>
     <message>
         <location filename="../../src/maindialog.ui" line="572"/>
         <source>The lowest virtual terminal number that will be used</source>
-        <translation>Nejnižší číslo virtuálního terminálu, který bude použit</translation>
+        <translation>O número mínimo de terminais virtuais que serão usados</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="669"/>
         <source>Path to X server binary</source>
-        <translation>Popis umístění spustitelného souboru s X serverem</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="683"/>
         <source>Arguments passed to the X server invocation</source>
-        <translation>Argumenty předané vyvolání X serveru</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="690"/>
         <source>Directory containing available X sessions</source>
-        <translation>Složka obsahující X relace k dispozici</translation>
+        <translation>Diretório contendo sessões do X disponíveis</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="697"/>
         <source>Path to a script to execute when starting the display server</source>
-        <translation>Popis umístění skriptu který vykonat při spouštění zobrazovacího serveru</translation>
+        <translation type="unfinished">Caminho para um script a ser executado ao iniciar a tela de login</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="704"/>
         <source>Path to a script to execute when stopping the display server</source>
-        <translation>Popis umístění skriptu který vykonat při zastavování zobrazovacího serveru</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="714"/>
         <source>Path to xauth binary</source>
-        <translation>Popis umístění spouštěcího souboru s xauth</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="721"/>
         <source>Path to Xephyr binary</source>
-        <translation>Popis umístění spouštěcího souboru s Xephyr</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="747"/>
         <source>Path to the Xauthority file</source>
-        <translation>Popis umístění souboru Xauthority</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="759"/>
         <source>File</source>
-        <translation>Soubor</translation>
+        <translation>Arquivo</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.cpp" line="31"/>
+        <source>SDDM Configuration Editor</source>
+        <translation>Editor de configurações do SDDM</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="778"/>
         <source>About</source>
-        <translation>O aplikaci</translation>
+        <translation>Sobre</translation>
     </message>
     <message>
-        <location filename="../../src/maindialog.cpp" line="50"/>
-        <source>SDDM Configuration Editor</source>
-        <translation>Editor nastavení pro SDDM</translation>
-    </message>
-    <message>
-        <location filename="../../src/maindialog.cpp" line="93"/>
-        <source>Close %1 preview</source>
-        <translation>Zavřít náhled %1</translation>
-    </message>
-    <message>
-        <location filename="../../src/maindialog.cpp" line="108"/>
+        <location filename="../../src/maindialog.cpp" line="89"/>
         <source>Choose a file</source>
-        <translation>Zvolte soubor</translation>
+        <translation>Escolha um arquivo</translation>
     </message>
     <message>
-        <location filename="../../src/maindialog.cpp" line="115"/>
+        <location filename="../../src/maindialog.cpp" line="96"/>
         <source>Choose a directory</source>
-        <translation>Zvolte složku</translation>
+        <translation>Escolha um diretório</translation>
+    </message>
+    <message>
+        <location filename="../../src/maindialog.cpp" line="74"/>
+        <source>Close %1 preview</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -261,32 +260,32 @@ pokud nejsou výslovně zapnutí pomocí EnableAvatars</translation>
     <message>
         <location filename="../../src/dialogabout.ui" line="27"/>
         <source>Information</source>
-        <translation>Informace</translation>
+        <translation>Informação</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="43"/>
         <source>qrc:/about.html</source>
-        <translation>qrc:/about.html</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="62"/>
         <source>Thanks</source>
-        <translation>Poděkování</translation>
+        <translation>Agradecimentos</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="81"/>
         <source>qrc:/thanks.html</source>
-        <translation>qrc:/thanks.html</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="100"/>
         <source>License</source>
-        <translation>Licence</translation>
+        <translation>Licença</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="116"/>
         <source>qrc:/license.html</source>
-        <translation>qrc:/license.html</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.cpp" line="55"/>
@@ -296,7 +295,7 @@ pokud nejsou výslovně zapnutí pomocí EnableAvatars</translation>
     <message>
         <location filename="../../src/dialogabout.cpp" line="60"/>
         <source>About</source>
-        <translation>O aplikaci</translation>
+        <translation>Sobre</translation>
     </message>
 </context>
 </TS>
