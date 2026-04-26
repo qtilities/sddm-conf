31101059be9160126ac7e1a924f217c7c9258199
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2026-04-26 07:52:07 +0200

    Weblate commits (#60)
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/sddm-conf-menu-entry/kk/
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/sddm-conf/kab/
    Translate-URL: https://translate.lxqt-project.org/projects/qtilities/sddm-conf/kk/
    Translation: Qtilities/SDDM-conf
    Translation: Qtilities/SDDM-conf Menu Entry
    
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: Baurzhan Muftakhidinov <baurthefirst@gmail.com>
    Co-authored-by: ButterflyOfFire <butterflyoffire@protonmail.com>

--- resources/translations/sddm-conf_kk.ts
+++ resources/translations/sddm-conf_kk.ts
@@ -1,149 +1,151 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="kab">
+<TS version="2.1" language="kk">
 <context>
     <name>MainDialog</name>
     <message>
         <location filename="../../src/maindialog.ui" line="40"/>
         <source>Autologin</source>
-        <translation>Tuqqna tawurmant</translation>
+        <translation>Автокіру</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="61"/>
         <source>Username for autologin session</source>
-        <translation type="unfinished"></translation>
+        <translation>Автокіру сессиясы үшін пайдаланушы аты</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="71"/>
         <source>Whether sddm should automatically log back into sessions when they exit</source>
-        <translation type="unfinished"></translation>
+        <translation>Сессия аяқталғанда sddm оған автоматты түрде қайта кіру керек пе</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="84"/>
         <source>Name of session file for autologin session (if empty try last logged in)</source>
-        <translation type="unfinished"></translation>
+        <translation>Автокіру сессиясы үшін сессия файлының аты (бос болса, соңғы кірген сессия қолданылады)</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="108"/>
         <source>General</source>
-        <translation>Amatu</translation>
+        <translation>Жалпы</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="135"/>
         <source>Reboot command</source>
-        <translation type="unfinished"></translation>
+        <translation>Қайта жүктеу командасы</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="142"/>
         <source>If property is set to none, numlock won&apos;t be changed
 NOTE: Currently ignored if autologin is enabled.</source>
-        <translation type="unfinished"></translation>
+        <translation>Егер қасиет «none» мәніне орнатылса, NumLock өзгермейді
+ЕСКЕРТПЕ: Қазіргі уақытта автокіру іске қосылған болса, еленбейді.</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="172"/>
         <source>Initial NumLock state. Can be on, off or none.</source>
-        <translation type="unfinished"></translation>
+        <translation>NumLock-тың бастапқы күйі. «on», «off» немесе «none» болуы мүмкін.</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="179"/>
         <source>Input method module</source>
-        <translation type="unfinished"></translation>
+        <translation>Енгізу әдісінің модулі</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="186"/>
         <source>Halt command</source>
-        <translation type="unfinished"></translation>
+        <translation>Тоқтату командасы</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="207"/>
         <source>Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Тема</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="250"/>
         <source>Theme directory path</source>
-        <translation type="unfinished"></translation>
+        <translation>Тема бумасының жолы</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="257"/>
         <source>above which avatars are disabled
 unless explicitly enabled with EnableAvatars</source>
-        <translation type="unfinished"></translation>
+        <translation>одан асса, аватарлар сөндіріледі,
+егер EnableAvatars арқылы арнайы іске қосылмаса</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="265"/>
         <source>Current theme name</source>
-        <translation type="unfinished"></translation>
+        <translation>Ағымдағы тема аты</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="272"/>
         <source>Cursor theme used in the greeter</source>
-        <translation type="unfinished"></translation>
+        <translation>Қарсы алу терезесінде қолданылатын курсор темасы</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="279"/>
         <source>Global directory for user avatars</source>
-        <translation type="unfinished"></translation>
+        <translation>Пайдаланушы аватарларына арналған глобалды бума</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="289"/>
         <source>Number of users to use as threshold</source>
-        <translation type="unfinished"></translation>
+        <translation>Шекті мән ретінде қолданылатын пайдаланушылар саны</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="302"/>
         <source>Preview</source>
-        <translation>Taskant</translation>
+        <translation>Алдын ала қарау</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="309"/>
         <source>Enable display of custom user avatars</source>
-        <translation type="unfinished"></translation>
+        <translation>Пайдаланушының таңдаулы аватарларын көрсетуді іске қосу</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="316"/>
         <source>The files should be named &lt;username&gt;.face.icon</source>
-        <translation type="unfinished"></translation>
+        <translation>Файлдар &lt;username&gt;.face.icon түрінде аталуы керек</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="324"/>
         <source>Users</source>
-        <translation>Iseqdacen</translation>
+        <translation>Пайдаланушылар</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="345"/>
         <source>Default $PATH for logged in users</source>
-        <translation type="unfinished"></translation>
+        <translation>Жүйеге кірген пайдаланушылар үшін бастапқы $PATH</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="355"/>
         <source>Comma-separated list of shells</source>
-        <translation type="unfinished"></translation>
+        <translation>Үтірмен ажыратылған қоршамдар тізімі</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="365"/>
         <source>Comma-separated list of users that should not be listed</source>
-        <translation type="unfinished"></translation>
+        <translation>Тізімде көрсетілмеуі тиіс пайдаланушылардың үтірмен ажыратылған тізімі</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="375"/>
         <source>Minimum user id for displayed users</source>
-        <translation type="unfinished"></translation>
+        <translation>Көрсетілетін пайдаланушылардың минималды ID-і</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="392"/>
         <source>Maximum user id for displayed users</source>
-        <translation type="unfinished"></translation>
+        <translation>Көрсетілетін пайдаланушылардың максималды ID-і</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="409"/>
         <source>Remember the session of the last successfully logged in user</source>
-        <translation type="unfinished"></translation>
+        <translation>Соңғы сәтті кірген пайдаланушының сессиясын есте сақтау</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="416"/>
         <source>Remember the last successfully logged in user</source>
-        <translation type="unfinished"></translation>
+        <translation>Соңғы сәтті кірген пайдаланушыны есте сақтау</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="424"/>
@@ -154,24 +156,24 @@ unless explicitly enabled with EnableAvatars</source>
         <location filename="../../src/maindialog.ui" line="445"/>
         <location filename="../../src/maindialog.ui" line="565"/>
         <source>Enable Qt&apos;s automatic high-DPI scaling</source>
-        <translation type="unfinished"></translation>
+        <translation>Qt-тың автоматты жоғары DPI масштабтауын іске қосу</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="452"/>
         <location filename="../../src/maindialog.ui" line="676"/>
         <source>Path to a script to execute when starting the desktop session</source>
-        <translation type="unfinished"></translation>
+        <translation>Жұмыс үстелі сессиясын бастау кезінде орындалатын скрипт жолы</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="470"/>
         <location filename="../../src/maindialog.ui" line="740"/>
         <source>Path to the user session log file</source>
-        <translation type="unfinished"></translation>
+        <translation>Пайдаланушы сессиясы журналы файлының жолы</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="488"/>
         <source>Directory containing available Wayland sessions</source>
-        <translation type="unfinished"></translation>
+        <translation>Қолжетімді Wayland сессиялары бар бума</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="520"/>
@@ -181,77 +183,77 @@ unless explicitly enabled with EnableAvatars</source>
     <message>
         <location filename="../../src/maindialog.ui" line="572"/>
         <source>The lowest virtual terminal number that will be used</source>
-        <translation type="unfinished"></translation>
+        <translation>Қолданылатын ең кіші виртуалды терминал нөмірі</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="669"/>
         <source>Path to X server binary</source>
-        <translation type="unfinished"></translation>
+        <translation>X-сервердің бинарлы файлының жолы</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="683"/>
         <source>Arguments passed to the X server invocation</source>
-        <translation type="unfinished"></translation>
+        <translation>X-серверді іске қосқанда берілетін аргументтер</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="690"/>
         <source>Directory containing available X sessions</source>
-        <translation type="unfinished"></translation>
+        <translation>Қолжетімді X сессиялары бар бума</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="697"/>
         <source>Path to a script to execute when starting the display server</source>
-        <translation type="unfinished"></translation>
+        <translation>Дисплей серверін іске қосқанда орындалатын скрипт жолы</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="704"/>
         <source>Path to a script to execute when stopping the display server</source>
-        <translation type="unfinished"></translation>
+        <translation>Дисплей серверін тоқтатқанда орындалатын скрипт жолы</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="714"/>
         <source>Path to xauth binary</source>
-        <translation type="unfinished"></translation>
+        <translation>xauth бинарлы файлының жолы</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="721"/>
         <source>Path to Xephyr binary</source>
-        <translation type="unfinished"></translation>
+        <translation>Xephyr бинарлы файлының жолы</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="747"/>
         <source>Path to the Xauthority file</source>
-        <translation type="unfinished"></translation>
+        <translation>Xauthority файлының жолы</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="759"/>
         <source>File</source>
-        <translation>Afaylu</translation>
+        <translation>Файл</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.cpp" line="31"/>
         <source>SDDM Configuration Editor</source>
-        <translation type="unfinished"></translation>
+        <translation>SDDM конфигурация редакторы</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.ui" line="778"/>
         <source>About</source>
-        <translation>Ɣef</translation>
+        <translation>Осы туралы</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.cpp" line="89"/>
         <source>Choose a file</source>
-        <translation>Fren afaylu</translation>
+        <translation>Файлды таңдау</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.cpp" line="96"/>
         <source>Choose a directory</source>
-        <translation>Fren akaram</translation>
+        <translation>Буманы таңдау</translation>
     </message>
     <message>
         <location filename="../../src/maindialog.cpp" line="74"/>
         <source>Close %1 preview</source>
-        <translation>Mdel taskant %1</translation>
+        <translation>%1 алдын ала қарауын жабу</translation>
     </message>
 </context>
 <context>
@@ -259,7 +261,7 @@ unless explicitly enabled with EnableAvatars</source>
     <message>
         <location filename="../../src/dialogabout.ui" line="27"/>
         <source>Information</source>
-        <translation>Talɣut</translation>
+        <translation>Ақпарат</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="43"/>
@@ -269,7 +271,7 @@ unless explicitly enabled with EnableAvatars</source>
     <message>
         <location filename="../../src/dialogabout.ui" line="62"/>
         <source>Thanks</source>
-        <translation>Asnemmer</translation>
+        <translation>Алғыс</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="81"/>
@@ -279,7 +281,7 @@ unless explicitly enabled with EnableAvatars</source>
     <message>
         <location filename="../../src/dialogabout.ui" line="100"/>
         <source>License</source>
-        <translation>Turagt</translation>
+        <translation>Лицензия</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.ui" line="116"/>
@@ -289,12 +291,12 @@ unless explicitly enabled with EnableAvatars</source>
     <message>
         <location filename="../../src/dialogabout.cpp" line="55"/>
         <source>Author</source>
-        <translation>Ameskar</translation>
+        <translation>Автор</translation>
     </message>
     <message>
         <location filename="../../src/dialogabout.cpp" line="60"/>
         <source>About</source>
-        <translation>Ɣef</translation>
+        <translation>Осы туралы</translation>
     </message>
 </context>
 </TS>
